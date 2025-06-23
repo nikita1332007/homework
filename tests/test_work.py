bd22ea94b49883d03154163d029c14e25a8a3360
nikita <nikilodion87@gmail.com>
2025-06-23 22:24:02 +0300

    src

--- tests/test_work.py
+++ tests/test_work.py
@@ -1,58 +1,43 @@
 import unittest
 
-
-class Product:
-    def __init__(self, name, description, price, quantity):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
+from src.work import Product, Category
 
 
-class Category:
-    category_count = 0
-    product_count = 0
-
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
-        self.products = []
-        Category.category_count += 1
-
-    def add_product(self, product):
-        self.products.append(product)
-        Category.product_count += 1
-
-
-class TestProductCategory(unittest.TestCase):
+class TestProductAndCategory(unittest.TestCase):
 
     def setUp(self):
-        self.product1 = Product("Samsung Galaxy S23", "Latest model", 180000.0, 5)
-        self.product2 = Product("iPhone 15", "New generation", 210000.0, 8)
-        self.category = Category("Smartphones", "Devices for communication and more")
+        self.product1 = Product("Товар 1", "Описание 1", 100, 10)
+        self.product2 = Product("Товар 2", "Описание 2", 200, 5)
+        self.category = Category("Категория 1", "Описание категории", [self.product1, self.product2])
 
-    def test_product_creation(self):
-        self.assertEqual(self.product1.name, "Samsung Galaxy S23")
-        self.assertEqual(self.product1.price, 180000.0)
-        self.assertEqual(self.product1.quantity, 5)
+    def test_product_initialization(self):
+        self.assertEqual(self.product1.name, "Товар 1")
+        self.assertEqual(self.product1.price, 100)
+        self.assertEqual(self.product1.quantity, 10)
 
-    def test_category_creation(self):
-        self.assertEqual(self.category.name, "Smartphones")
-        self.assertEqual(self.category.description, "Devices for communication and more")
-        self.assertEqual(len(self.category.products), 0)
+    def test_set_price(self):
+        self.product1.price = 150
+        self.assertEqual(self.product1.price, 150)
 
-    def test_add_product(self):
-        self.category.add_product(self.product1)
-        self.assertEqual(len(self.category.products), 1)
-        self.assertEqual(Category.product_count, 1)
+    def test_set_price_negative(self):
+        with self.assertRaises(ValueError):
+            self.product1.price = -50
 
-        self.category.add_product(self.product2)
-        self.assertEqual(len(self.category.products), 2)
-        self.assertEqual(Category.product_count, 2)
+    def test_new_product(self):
+        products_list = []
+        new_product = Product.new_product({"name": "Товар 3", "description": "Описание 3", "price": 300, "quantity": 2}, products_list)
+        self.assertEqual(len(products_list), 1)
+        self.assertEqual(new_product.name, "Товар 3")
 
-    def test_category_count(self):
-        self.assertEqual(Category.category_count, 2)
+    def test_add_product_to_category(self):
+        new_product = Product("Товар 3", "Описание 3", 300, 2)
+        self.category.add_product(new_product)
+        self.assertEqual(len(self.category.products), 3)
+
+    def test_invalid_product_in_category(self):
+        with self.assertRaises(ValueError):
+            self.category.add_product("Некорректный продукт")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
